7dab3122199d2e1bfbe6b53f1c566fbfc61f6488
Honahuku <tech@honahuku.com>
2023-05-17 12:49:03 +0900

    feat: disable SecureNAT

--- image/softether/README.md
+++ image/softether/README.md
@@ -1,12 +1,12 @@
 ### build & push
 ```sh
 cd image/
-docker image build --progress=plain -t ghcr.io/honahuku/softether:"4.38.9760.v7" -f Dockerfile --build-arg SERVER_PASS=xxx .
+docker image build --progress=plain -t ghcr.io/honahuku/softether:"4.38.9760.v8" -f Dockerfile --build-arg SERVER_PASS=xxx .
 # ghcr.ioにログインしていることを確認する
-docker push ghcr.io/honahuku/softether:"4.38.9760.v7"
+docker push ghcr.io/honahuku/softether:"4.38.9760.v8"
 ```
 ### run
 ```sh
-docker run -it -p 443:443 ghcr.io/honahuku/softether:"4.38.9760.v7"
+docker run -it -p 443:443 ghcr.io/honahuku/softether:"4.38.9760.v8"
 docker stop $(docker ps -q)
 ```
--- image/softether/start.sh
+++ image/softether/start.sh
@@ -19,21 +19,21 @@ sleep 2
 # ローカルブリッジ接続の作成
 /usr/local/vpnserver/vpncmd /SERVER localhost /PASSWORD "$SERVER_PASS" /cmd BridgeCreate DEFAULT /DEVICE:soft /TAP:yes
 
-# 匿名ユーザーへの列挙の禁止
-/usr/local/vpnserver/vpncmd /SERVER localhost /PASSWORD "$SERVER_PASS" /HUB:DEFAULT /cmd SetEnumDeny
-# SecureNATの有効化
-/usr/local/vpnserver/vpncmd /SERVER localhost /PASSWORD "$SERVER_PASS" /HUB:DEFAULT /cmd SecureNatEnable
-# SecureNATのNAT機能の有効化
-/usr/local/vpnserver/vpncmd /SERVER localhost /PASSWORD "$SERVER_PASS" /HUB:DEFAULT /cmd NatEnable
-# SecureNATのDHCP機能の有効化
-/usr/local/vpnserver/vpncmd /SERVER localhost /PASSWORD "$SERVER_PASS" /HUB:DEFAULT /cmd DhcpEnable
+# # 匿名ユーザーへの列挙の禁止
+# /usr/local/vpnserver/vpncmd /SERVER localhost /PASSWORD "$SERVER_PASS" /HUB:DEFAULT /cmd SetEnumDeny
+# # SecureNATの有効化
+# /usr/local/vpnserver/vpncmd /SERVER localhost /PASSWORD "$SERVER_PASS" /HUB:DEFAULT /cmd SecureNatEnable
+# # SecureNATのNAT機能の有効化
+# /usr/local/vpnserver/vpncmd /SERVER localhost /PASSWORD "$SERVER_PASS" /HUB:DEFAULT /cmd NatEnable
+# # SecureNATのDHCP機能の有効化
+# /usr/local/vpnserver/vpncmd /SERVER localhost /PASSWORD "$SERVER_PASS" /HUB:DEFAULT /cmd DhcpEnable
 
-# SecureNATのネットワークインターフェース設定の変更
-# クラスA、/14のネットワークとする
-# https://www.mrl.co.jp/download/manual-online/gl2000/gl2000_02/manual/docs/netlista.htm#list
-/usr/local/vpnserver/vpncmd /SERVER localhost /PASSWORD "$SERVER_PASS" /HUB:DEFAULT /cmd SecureNatHostSet /MAC:none /IP:10.200.0.1 /MASK:255.252.0.0
-# SecureNATのDHCP設定
-/usr/local/vpnserver/vpncmd /SERVER localhost /PASSWORD "$SERVER_PASS" /HUB:DEFAULT /cmd DhcpSet /START:10.200.0.2 /END:10.203.255.255 /MASK:255.252.0.0 /EXPIRE:86400 /GW:10.200.0.1 /DNS:"$KUBE_DNS" /DNS2:1.1.1.1 /DOMAIN:none /LOG:yes
+# # SecureNATのネットワークインターフェース設定の変更
+# # クラスA、/14のネットワークとする
+# # https://www.mrl.co.jp/download/manual-online/gl2000/gl2000_02/manual/docs/netlista.htm#list
+# /usr/local/vpnserver/vpncmd /SERVER localhost /PASSWORD "$SERVER_PASS" /HUB:DEFAULT /cmd SecureNatHostSet /MAC:none /IP:10.200.0.1 /MASK:255.252.0.0
+# # SecureNATのDHCP設定
+# /usr/local/vpnserver/vpncmd /SERVER localhost /PASSWORD "$SERVER_PASS" /HUB:DEFAULT /cmd DhcpSet /START:10.200.0.2 /END:10.203.255.255 /MASK:255.252.0.0 /EXPIRE:86400 /GW:10.200.0.1 /DNS:"$KUBE_DNS" /DNS2:1.1.1.1 /DOMAIN:none /LOG:yes
 
 # ユーザーの作成
 /usr/local/vpnserver/vpncmd /SERVER localhost /PASSWORD "$SERVER_PASS" /HUB:DEFAULT /cmd UserCreate honahuku /GROUP:none /REALNAME:none /NOTE:none
